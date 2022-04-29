2cea64dea577505c4f21d50938f2dc974f3cf8bd
Renaldo Stammerjohann <renaldo@pastoraldacrianca.org.br>
2022-04-29 16:01:06 -0300

    criacao projeto

---
+++ www/js/script.js
@@ -0,0 +1,35 @@
+function somar(){
+
+    let numero1 = document.getElementById("firstnum").value;
+    let numero2 = document.getElementById("secundnum").value;
+    let res= parseInt(numero1) + parseInt(numero2);
+
+
+   document.querySelector('#resultado').innerHTML= res;
+
+}
+
+let btnsomar = document.getElementById("btnSomar");
+btnsomar.addEventListener('click', somar);
+
+
+$("#tela").on('click', function(){
+  function rede(){
+    var network = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+    
+   alert('Connection type: ' + states[network]);
+
+  }
+  rede();
+
+}); 
