f4f3a9431ab5ae9fd7bd4a947baa1678a2a30a62
Renaldo Stammerjohann <renaldo@pastoraldacrianca.org.br>
2022-05-24 15:46:57 -0300

    feitoria

--- www/js/lugar.js
+++ www/js/lugar.js
@@ -3,11 +3,7 @@ document.getElementById("botao").addEventListener("click", tudo);
 
 function tudo() {
 
-<<<<<<< HEAD
     console.log("ola, bom dia javascript")
-=======
-    console.log("ola, bom dia javascript");
->>>>>>> parent of 5fbda47 (Revert "Revert "Revert "atualizado""")
 } 
 
 
